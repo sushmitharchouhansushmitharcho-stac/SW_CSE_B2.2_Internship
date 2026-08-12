e75585b4d17efbbdf90ea36a843304ce16c3666b
sushmitharchouhansush <sushmitharchouhansushmitharcho@gmail.com>
2026-08-12 10:57:10 +0530

    JavaScript(JS) usage

---
+++ Day/Day8/P1.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+  const myButton= document.getElementById('internal-js');
+
+          myButton.addEventListener('click',function(){
+            alert('Hello Learners, Good morning, This is intenal JS')
+          });
+});
