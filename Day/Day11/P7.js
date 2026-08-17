5048db75b411603419093bc1df14203c2d3db3a3
sushmitharchouhansush <sushmitharchouhansushmitharcho@gmail.com>
2026-08-17 13:14:57 +0530

    Async Keyword and await keyword

---
+++ Day/Day11/P7.js
@@ -0,0 +1,19 @@
+// Async keyword: this keyword makes a synchronous function into
+// an asynchronous code 
+
+async function myFunction(){
+    return 42;
+}
+const result = myFunction();
+console.log(result);
+
+// await keyword: pauses the execution of the async function
+//  untill the promise reaches either the resolve state or reject state
+
+async function getData() {
+    const promise = promise.resolve("Hello");
+
+    const value = await promise;
+    console.log(value);
+}
+getData();
